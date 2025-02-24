d30ea806ad02748c13b3cc11ad303a8b3770be22
maplesugano <ks2222@ic.ac.uk>
2025-02-24 20:11:20 +0000

    Added new allow origins

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://irac-with-rag-0eb67bd7c93c.herokuapp.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
